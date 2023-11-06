a7a6874330ecd03d88a90904dee0ac1f28a3a417
AjayKarthikBoopathy <ajaykarthikbak97@gmail.com>
2023-11-06 09:04:54 +0530

    first-commit

---
+++ models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,   //required true removed for signing in with Google Account
+    },
+    img: {
+      type: String,
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    subscribedUsers: {
+      type: [String],
+    },
+    fromGoogle: {
+      type: Boolean,
+      default: false,   //data changes when signed in with Google
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
